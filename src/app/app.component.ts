ea7033aee9d81cf60b1387e37e0631b12cb5b1d2
Grzegorz Kossowski <grzegorz.kossowski@gmail.com>
2022-07-11 21:05:07 +0200

    pipes simpified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-tutorial-pipes';
+  name: string = 'john doe';
+  date: string = ''
+  amount: number = 0
+
+  onNameChange({ target }: Event) {
+    const { value } = target as HTMLInputElement;
+    this.name = value;
+  }
+  onDateChange({ target }: Event) {
+    const { value } = target as HTMLInputElement;
+    this.date = value;
+  }
+  onAmountChange({ target }: Event) {
+    const { value } = target as HTMLInputElement;
+    this.amount = parseInt(value);
+  }
 }
